532f1743573a66646ee56d3e5294300a27e5d209
Lenny Xue <xuequnmou@163.com>
2013-01-09 12:05:16 +0800

    test stash

---
+++ testForStash.groovy
@@ -0,0 +1,10 @@
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Administrator
+ * Date: 13-1-6
+ * Time: 下午11:26
+ * To change this template use File | Settings | File Templates.
+ */
+
+added by master
